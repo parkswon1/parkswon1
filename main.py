a6b1219f774c588c1f44b8195709d5e6bbfead0b
parkseokwon <parkswon1@naver.com>
2024-08-15 23:24:44 +0900

    블로그 글들을 개별 카드로 만들기

--- main.py
+++ main.py
@@ -4,14 +4,24 @@ URL = "https://naturecancoding.tistory.com/rss"
 RSS_FEED = feedparser.parse(URL)
 MAX_POST = 7
 
-new_content = "### 📝 최신 블로그 포스트\n\n"  # 블로그 포스트 리스트 앞에 제목 추가
+new_content = """
+<div style="display: flex; flex-wrap: wrap; justify-content: space-around;">
+"""  # 카드들을 감싸는 div 시작
 
 for idx, feed in enumerate(RSS_FEED['entries']):
     if idx >= MAX_POST:
         break
     else:
         feed_date = feed['published_parsed']
-        new_content += f"- 📰 [{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']})\n"
+        new_content += f"""
+        <div style="border: 1px solid #ddd; border-radius: 8px; width: 300px; padding: 16px; margin: 8px; box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.1);">
+            <h3>{feed['title']}</h3>
+            <p style="color: #555;">{time.strftime('%Y/%m/%d', feed_date)}</p>
+            <a href="{feed['link']}" style="text-decoration: none; color: #1E90FF;">Read More</a>
+        </div>
+        """  # 개별 카드
+
+new_content += "</div>"  # 카드들을 감싸는 div 종료
 
 def update_readme_section(new_content):
     with open("README.md", "r", encoding="utf-8") as file:
