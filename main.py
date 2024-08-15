c26ef86642853df7508ae7c16ab73b7f8f21855d
parkseokwon <parkswon1@naver.com>
2024-08-15 23:18:09 +0900

    특정 섹션에만 적히도록 변경

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import feedparser, time
 
-URL="https://naturecancoding.tistory.com/rss"
+URL = "https://naturecancoding.tistory.com/rss"
 RSS_FEED = feedparser.parse(URL)
-MAX_POST=7
+MAX_POST = 7
 
-markdown_text = """
-[쓰고싶은 내용]
-""" # list of blog posts will be appended here
-
+new_content = ""  # list of blog posts will be appended here
 
 for idx, feed in enumerate(RSS_FEED['entries']):
-    if idx > MAX_POST:
+    if idx >= MAX_POST:
         break
     else:
         feed_date = feed['published_parsed']
-        markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
+        new_content += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
 
-f = open("README.md",mode="w", encoding="utf-8")
-f.write(markdown_text)
-f.close()
+def update_readme_section(new_content):
+    with open("README.md", "r", encoding="utf-8") as file:
+        lines = file.readlines()
+
+    start_index = None
+    end_index = None
+    for i, line in enumerate(lines):
+        if "<!-- START_CUSTOM_SECTION -->" in line:
+            start_index = i
+        elif "<!-- END_CUSTOM_SECTION -->" in line:
+            end_index = i
+            break
+
+    if start_index is not None and end_index is not None:
+        # Keep the section before and after the custom section and replace the content in between
+        new_lines = lines[:start_index+1] + [new_content + "\n"] + lines[end_index:]
+        with open("README.md", "w", encoding="utf-8") as file:
+            file.writelines(new_lines)
+
+if __name__ == "__main__":
+    update_readme_section(new_content)
